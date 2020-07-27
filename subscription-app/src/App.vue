81cf055c6176fbeaa5148de0af48a82c431b4b1e
Taylor Cooke <taylorcooke@gmail.com>
2020-07-27 13:54:19 -0400

    hitting same router error

--- subscription-app/src/App.vue
+++ subscription-app/src/App.vue
@@ -1,28 +1,12 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <h2><router-link to="/index">Subscriptions</router-link></h2>
+        <router-link to="/create" class="btn btn-sm btn-primary">Add subscription</router-link>
+
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
